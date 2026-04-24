fe00038388013c3028b0f8e6ab19202e72cebd37
Ramon Cerqueira <ramonssa100@gmail.com>
2026-04-24 16:01:55 -0300

    feat: update project configuration and generate build-related server artifacts

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JC8aLMUDP6xd/u6FZBJFHzBO+Wg3Lpue9tZZX3dzefA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"WNl5QZ+aFJQ/60TsSYiHPMZbpuknHGWRg7WV48vuVfE=\"}"
